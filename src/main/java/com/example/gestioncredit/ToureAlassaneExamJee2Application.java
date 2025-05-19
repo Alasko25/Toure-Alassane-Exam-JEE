6e7eec66d8baab34f9fa8e748a1a6cb50fec509f
Alasko25 <alassane.toure-etu@etu.univh2c.ma>
2025-05-19 09:31:35 +0100

    Initial commit - projet Spring Boot

---
+++ src/main/java/com/example/gestioncredit/ToureAlassaneExamJee2Application.java
@@ -0,0 +1,13 @@
+package com.example.gestioncredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToureAlassaneExamJee2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToureAlassaneExamJee2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestioncredit/ToureAlassaneExamJee2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestioncredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToureAlassaneExamJee2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
